e6007f1deeb1fdd8b7f4d1e18e2a2894c9cab288
slabo157 <labonte.scott@gmail.com>
2017-04-11 10:08:22 -0400

    Added scripts folder with user authentication, added to index.html
    Added user authentication fieldsets to index.html and added scripts
    folder from game-api to my project folder which includes all user
    authentication and changed local host on all scripts from
    localhost:3000 to localhost:4741.

---
+++ scripts/change-password.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl --include --request PATCH http://localhost:4741/change-password/$ID \
+  --header "Authorization: Token token=$TOKEN" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "an example password",
+      "new": "super sekrit"
+    }
+  }'
